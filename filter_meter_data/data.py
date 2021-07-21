8e6c9a589d8785e580e1954665d783cd029986bc
Brandon <phizzl3@gmail.com>
2021-07-21 17:24:42 -0500

    Update data.py Adds multiple variables for import in other files

--- filter_meter_data/data.py
+++ filter_meter_data/data.py
@@ -1,1 +1,23 @@
 # Variables
+
+from pathlib import Path
+
+
+# Source file search expression
+SOURCE_FILENAME = r'Meter_Reads_Data_Dump.*\.xlsx'
+
+# Folder to search for source files (User's Downloads folder)
+FOLDER = Path().home().resolve() / "Downloads"
+
+# Columns from source file containing the needed info
+KEEP_COLUMNS = ('A', 'B', 'C', 'D', 'E', 'F', 'H', 
+                'J', 'K', 'N', 'P', 'BF', 'BK', 'CC')
+
+# Value to verify to check that we're using the correct source file
+CHECK_VALUE = 'Meter Read Data Dump'
+
+# Cell to verify info for CHECK_VALUE
+VALUE_CELL = 'A1'
+
+# Cell containing date range info
+DATE_CELL = 'A2'
