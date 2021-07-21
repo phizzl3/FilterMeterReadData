33ecbf8f0cd7f7c9c2edfd30b41e91b9a1e589a3
Brandon <phizzl3@gmail.com>
2021-07-21 17:25:19 -0500

    Update get_date.py Initial code complete

--- filter_meter_data/get_date.py
+++ filter_meter_data/get_date.py
@@ -1,1 +1,14 @@
 # Get formatted date range from source file
+
+from xlclass import Xlsx
+
+
+def get_date(xl: Xlsx, cell: str) -> str:
+    
+    # Get date range and return formatted string
+    date = xl.ws[cell].value
+    date = date.strip('Date Range: ')
+    date = date.replace('/', '.')
+    date = date.replace(' ', '')
+    
+    return date
