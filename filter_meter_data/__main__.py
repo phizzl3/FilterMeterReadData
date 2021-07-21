26c622695a9f1a414e78754d114318574103848a
Brandon <phizzl3@gmail.com>
2021-07-21 17:26:33 -0500

    Update __main__.py Adds get source files, generate Xlsx, verify value, and get date range.

--- filter_meter_data/__main__.py
+++ filter_meter_data/__main__.py
@@ -16,3 +16,37 @@
 I'm breaking this up into a bunch of modules/files just to experiment. 
 So, enjoy. ;-)
 """
+
+from re_file_search import get_list
+from data import SOURCE_FILENAME, FOLDER, CHECK_VALUE, VALUE_CELL, DATE_CELL
+import dropfile
+from xlclass import Xlsx
+from verify_source import verify_value
+from time import sleep
+from get_date import get_date
+
+
+if __name__ == '__main__':
+    # Get list of source files from folder
+    source_files = get_list(FOLDER, SOURCE_FILENAME)
+    
+    # Ask for file location if none are found (single list item)
+    if not source_files:
+        source_files = [dropfile.get()]
+        
+    for file in source_files:
+        # Generate objects
+        source_xl = Xlsx(file)
+        target_xl = Xlsx()
+        
+        # Check source for correct contents
+        if not verify_value(source_xl, CHECK_VALUE, VALUE_CELL):
+            print(f"File contents don't match for:\n{file}\nPlease verify.")
+            sleep(2)
+            continue
+            
+        # Get formatted dates
+        date_range = get_date(source_xl, DATE_CELL)
+        
+        pass
+        
