62ae58aec85cd9f22d8fa6042e7a96eded7e616a
Brandon <phizzl3@gmail.com>
2021-07-21 17:25:05 -0500

    Update verify_source.py Initial code complete

--- filter_meter_data/verify_source.py
+++ filter_meter_data/verify_source.py
@@ -1,1 +1,10 @@
 # Check source file for correct info
+
+from xlclass import Xlsx
+
+
+def verify_value(xl: Xlsx, val: str, cell: str) -> bool:
+    
+    # Verify that the data in the passed cell matches the 
+    # value we're looking for.
+    return xl.ws[cell].value == val
