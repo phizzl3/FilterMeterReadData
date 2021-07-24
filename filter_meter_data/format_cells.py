c2973c79f27f6cdd2d079c06f7b14ef0251c25a7
phizzl3 <phizzl3@gmail.com>
2021-07-24 15:58:38 -0500

    Update format_cells.py Finishes _highlight_rows function.

--- filter_meter_data/format_cells.py
+++ filter_meter_data/format_cells.py
@@ -1,4 +1,6 @@
-from xlclass import Xlsx, COLORS
+from copy import copy
+
+from xlclass import COLORS, Xlsx
 
 
 def _update_title_cell(out_xl: Xlsx, find_replace: dict) -> None:
@@ -25,17 +27,28 @@ def _set_column_widths(out_xl: Xlsx, col_settings: dict) -> None:
         the size of the specified cells.
     """
     out_xl.set_cell_size(col_settings)
-    
-    
-def _highlight_rows(out_xl: Xlsx) -> None:
+
+
+def _highlight_rows(out_xl: Xlsx, startrow: int = 1) -> None:
+    """
+    Highlights alternating rows starting at startrow until the end of the 
+    sheet, unless it hits a row with 'Grand Total' in cell column 'A'.
+
+    Args:
+        out_xl (Xlsx): Object containing the cells to highlight
+        startrow (int, optional): Row number where highlighting should begin.
+        Defaults to 1.
+    """
+    highlight_row = copy(startrow)
     for row_number, row in enumerate(out_xl.ws.iter_rows(), 1):
-        if row_number < 5:
+        if row_number < startrow:
             continue
         if 'Grand Total' in out_xl.ws[f'A{row_number}'].value:
             break
-        if row_number % 2 != 0:
+        if row_number == highlight_row:
             for cell in row:
                 cell.fill = COLORS.get('gray')
+            highlight_row += 2
 
 
 def format_cells(out_xl: Xlsx, find_replace: dict, col_settings: dict) -> None:
@@ -52,4 +65,4 @@ def format_cells(out_xl: Xlsx, find_replace: dict, col_settings: dict) -> None:
     """
     _update_title_cell(out_xl, find_replace)
     _set_column_widths(out_xl, col_settings)
-    _highlight_rows(out_xl)
+    _highlight_rows(out_xl, startrow=5)
