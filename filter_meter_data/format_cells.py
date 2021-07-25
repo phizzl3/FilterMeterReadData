0a40be87a4c8978d0121d46ea20243793d7c8bee
phizzl3 <phizzl3@gmail.com>
2021-07-25 10:36:47 -0500

    Update format_cells.py Adds _total_combined_meters to get the combined totals for the last col.

--- filter_meter_data/format_cells.py
+++ filter_meter_data/format_cells.py
@@ -72,6 +72,25 @@ def _set_bold_cells(out_xl: Xlsx, startrow: int = 1, stoprow: int = 5) -> None:
             cell.font = Font(bold=True)
 
 
+def _total_combined_meters(out_xl: Xlsx, startrow: int = 5) -> None:
+    """
+    Adds up the last row and sets the final amount to the last cell in 
+    the column.
+
+    Args:
+        out_xl (Xlsx): Object containing the amounts to be totaled. 
+        startrow (int, optional): Row number to start adding. Defaults to 5.
+    """
+    total_meter = 0
+    for row_number, row in enumerate(out_xl.ws.iter_rows(), 1):
+        if row_number < startrow:
+            continue
+        if 'Grand Total' in out_xl.ws[f'A{row_number}'].value:
+            out_xl.ws[f'N{row_number}'] = total_meter
+            break
+        total_meter += out_xl.ws[f'N{row_number}'].value
+        
+
 def format_cells(out_xl: Xlsx, find_replace: dict, col_settings: dict) -> None:
     """
     Replaces the text in the specified cell with a new value and adjusts
@@ -88,3 +107,4 @@ def format_cells(out_xl: Xlsx, find_replace: dict, col_settings: dict) -> None:
     _set_column_widths(out_xl, col_settings)
     _highlight_rows(out_xl)
     _set_bold_cells(out_xl)
+    _total_combined_meters(out_xl)
